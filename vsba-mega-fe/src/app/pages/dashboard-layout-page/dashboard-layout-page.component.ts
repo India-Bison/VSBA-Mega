6522b0345ca9e275c789809b3ff99ed771a72ad6
Mayur Maruti Talekar <mayur.talekar@thecontrast.in>
2025-04-23 11:24:18 +0530

    dashbaord working succsefully

--- vsba-mega-fe/src/app/pages/dashboard-layout-page/dashboard-layout-page.component.ts
+++ vsba-mega-fe/src/app/pages/dashboard-layout-page/dashboard-layout-page.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { SidebarComponent } from '../../components/sidebar/sidebar.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard-layout-page',
   standalone: true,
-  imports: [],
+  imports: [SidebarComponent,RouterOutlet],
   templateUrl: './dashboard-layout-page.component.html',
   styleUrl: './dashboard-layout-page.component.css'
 })
