5f03750cfc092da3062578b81662a27476916405
Mayur Maruti Talekar <mayur.talekar@thecontrast.in>
2025-04-23 11:18:56 +0530

    dashbaord layout

--- vsba-mega-fe/src/app/app.routes.ts
+++ vsba-mega-fe/src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { DashboardLayoutPageComponent } from './pages/dashboard-layout-page/dashboard-layout-page.component';
+import { DashboardPageComponent } from './pages/dashboard-page/dashboard-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    // { path: 'sign-in', component: SignInPageComponent },
+
+    {
+        path: '', component: DashboardLayoutPageComponent,
+        // canActivate: [canActivateAuthRole],
+        children: [
+            { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+            { path: 'dashboard', component: DashboardPageComponent },
+         
+            { path: '**', redirectTo: 'dashboard' },
+
+
+        ]
+
+    }
+];
