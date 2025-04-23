5bd83f297c31a23aab5aebb3ce969bef526d4243
Mayur Maruti Talekar <mayur.talekar@thecontrast.in>
2025-04-23 11:14:36 +0530

    add filter compinet and dashbaord

---
+++ vsba-mega-fe/src/app/components/filter-toolbar/filter-toolbar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-filter-toolbar',
+  standalone: true,
+  imports: [],
+  templateUrl: './filter-toolbar.component.html',
+  styleUrl: './filter-toolbar.component.css'
+})
+export class FilterToolbarComponent {
+
+}
---
+++ vsba-mega-fe/src/app/pages/dashboard-layout-page/dashboard-layout-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-layout-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './dashboard-layout-page.component.html',
+  styleUrl: './dashboard-layout-page.component.css'
+})
+export class DashboardLayoutPageComponent {
+
+}
