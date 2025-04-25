42774e89fd75ea7a5bf9c2f27b1c1ce21eae134e
Mayur Maruti Talekar <mayur.talekar@thecontrast.in>
2025-04-25 14:20:23 +0530

    chnages

--- vsba-mega-fe/src/app/components/project-list/project-list.component.ts
+++ vsba-mega-fe/src/app/components/project-list/project-list.component.ts
@@ -2,10 +2,13 @@ import { Component } from '@angular/core';
 import { ListComponent } from '../list/list.component';
 import { HeaderComponent } from '../header/header.component';
 import { GlobalService } from '../../services/global.service';
+import { SearchInputComponent } from '../search-input/search-input.component';
+import { ToggleTabsComponent } from '../toggle-tabs/toggle-tabs.component';
+import { ButtonComponent } from '../button/button.component';
 
 @Component({
     selector: 'app-project-list',
-    imports: [ListComponent, HeaderComponent],
+    imports: [ListComponent, HeaderComponent,SearchInputComponent,ToggleTabsComponent,ButtonComponent],
     templateUrl: './project-list.component.html',
     styleUrl: './project-list.component.css',
     standalone: true,
@@ -21,5 +24,46 @@ constructor(public gs:GlobalService) {}
     { title: 'Slot Type', type: 'Value', key: 'slot_type', class: 'text-left' },
     { title: 'Start Date-End Date', type: 'startdate_enddate', key: 'project_start_date', class: 'text-left' },
     { title: 'Status', type: 'Value', key: 'status', class: 'text-left' },
+  ];
+  active_tab = 'All';
+  tabList = [
+    {
+      name: 'All',
+      action: () => {
+        this.active_tab = 'All',
+        console.log('All tab clicked')}
+    },
+    {
+      name: 'Pending',
+      action: () => {
+        this.active_tab = 'Pending';
+        console.log('Pending tab clicked');
+      }
+    },{
+      name: 'Approved',
+      action: () => {
+        this.active_tab = 'Approved';
+        console.log('Approved tab clicked');
+      }
+    },{
+      name: 'Reject',
+      action: () => {
+        this.active_tab = 'Reject';
+        console.log('Reject tab clicked');
+      }
+    },{
+      name: 'Disabled',
+      action: () => {
+        this.active_tab = 'Disabled';
+        console.log('Disabled tab clicked');
+      }
+    },
+    {
+      name: 'Drafts',
+      action: () => {
+        this.active_tab = 'Drafts';
+        console.log('Drafts tab clicked');
+      }
+    },
   ];
 }
