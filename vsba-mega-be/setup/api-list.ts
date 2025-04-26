751afc481a8fe8c1db80b058460f394ee7eb76d5
akashs25 <akashs25@gmail.com>
2025-04-26 22:12:51 +0530

    Changes

--- vsba-mega-be/setup/api-list.ts
+++ vsba-mega-be/setup/api-list.ts
@@ -1,6 +1,8 @@
 import { get_user_details } from "@config/apis/user/get-user/get-user.details";
+// Add Imports Here
 
 export let api_list: any[] = [
     //User
     get_user_details,
+    // Add Apis Here
 ]
