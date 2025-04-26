bacd5826b36d110c51ec137abc5507a8502bd6bb
akashs25 <akashs25@gmail.com>
2025-04-27 01:22:53 +0530

    Dummy Moved to Config

--- vsba-mega-be/setup/api-list.ts
+++ vsba-mega-be/setup/api-list.ts
@@ -1,10 +1,10 @@
 import { get_user_details } from "@config/apis/user/get-user/get-user.details";
 // Add Imports Here
-import { delete_dummy_details } from "@src/apis/dummy/delete-dummy/delete-dummy.details";
-import { get_dummy_list_details } from "@src/apis/dummy/get-dummy-list/get-dummy-list.details";
-import { get_dummy_details } from "@src/apis/dummy/get-dummy/get-dummy.details";
-import { update_dummy_details } from "@src/apis/dummy/update-dummy/update-dummy.details";
-import { create_dummy_details } from "@src/apis/dummy/create-dummy/create-dummy.details";
+import { delete_dummy_details } from "@config/generators/templates/dummy-crud/delete-dummy/delete-dummy.details";
+import { get_dummy_list_details } from "@config/generators/templates/dummy-crud/get-dummy-list/get-dummy-list.details";
+import { get_dummy_details } from "@config/generators/templates/dummy-crud/get-dummy/get-dummy.details";
+import { update_dummy_details } from "@config/generators/templates/dummy-crud/update-dummy/update-dummy.details";
+import { create_dummy_details } from "@config/generators/templates/dummy-crud/create-dummy/create-dummy.details";
 
 export let api_list: any[] = [
     //User
