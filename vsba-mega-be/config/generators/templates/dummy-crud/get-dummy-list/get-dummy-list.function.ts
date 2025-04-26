7f7d763186ed5df2a8b65ad4900fb506ed3b5e4a
akashs25 <akashs25@gmail.com>
2025-04-27 02:03:59 +0530

    Changes

--- vsba-mega-be/config/generators/templates/dummy-crud/get-dummy-list/get-dummy-list.function.ts
+++ vsba-mega-be/config/generators/templates/dummy-crud/get-dummy-list/get-dummy-list.function.ts
@@ -2,7 +2,6 @@ import { Error_Interface } from "@config/interfaces/error.interface";
 import { get_dummy_list_function_params, get_dummy_list_function_return } from "./get-dummy-list.interface";
 import { Op, Transaction } from "sequelize";
 import { Dummy } from "../dummy.model";
-import { User } from "@config/models/user.model";
 
 let get_dummy_list_function = async (data: get_dummy_list_function_params, transaction: Transaction): Promise<get_dummy_list_function_return | Error_Interface> => {
     const dummy_data = await Dummy.findAndCountAll({
