5948a2ae30ace32628423e2cd5aabbaf29f1ec2f
shubham-salunkhe-tc <shubham.salunkhe@thecontrast.in>
2025-04-27 13:13:50 +0530

    basic changes

--- vsba-mega-be/config/generators/templates/dummy-crud/get-dummy-list/get-dummy-list.function.ts
+++ vsba-mega-be/config/generators/templates/dummy-crud/get-dummy-list/get-dummy-list.function.ts
@@ -1,11 +1,11 @@
 import { Error_Interface } from "@config/interfaces/error.interface";
 import { get_dummy_list_function_params, get_dummy_list_function_return } from "./get-dummy-list.interface";
-import { Op, Transaction } from "sequelize";
-import { Dummy } from "../dummy.model";
+import { Transaction } from "sequelize";
 import { dummy_service } from "../dummy.service";
 
 let get_dummy_list_function = async (data: get_dummy_list_function_params, transaction: Transaction): Promise<get_dummy_list_function_return | Error_Interface> => {
     let filter = data.query
+
     const dummy_data = await dummy_service.get_all_dummy(filter, transaction);
     return {
         code: 200,
