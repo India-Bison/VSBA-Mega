f019d49a0a0a2982ce3f15ef5155447fac19b6ad
shubham-salunkhe-tc <shubham.salunkhe@thecontrast.in>
2025-04-27 13:12:57 +0530

    basic

--- vsba-mega-be/config/generators/templates/dummy-crud/delete-dummy/delete-dummy.function.ts
+++ vsba-mega-be/config/generators/templates/dummy-crud/delete-dummy/delete-dummy.function.ts
@@ -1,7 +1,6 @@
 import { Error_Interface } from "@config/interfaces/error.interface"
 import { delete_dummy_function_params, delete_dummy_function_return } from "./delete-dummy.interface"
-import { Op, Transaction } from "sequelize"
-import { Dummy } from "../dummy.model"
+import { Transaction } from "sequelize"
 import { dummy_service } from "../dummy.service"
 
 let delete_dummy_function = async (data: delete_dummy_function_params, transaction: Transaction): Promise<delete_dummy_function_return | Error_Interface> => {
