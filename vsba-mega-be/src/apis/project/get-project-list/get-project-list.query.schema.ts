6fdcf6c843b6925dfd0ebedd1f33dd733756ca1b
shubham-salunkhe-tc <shubham.salunkhe@thecontrast.in>
2025-05-03 10:07:38 +0530

    list api issue done filter, sorting, search given

--- vsba-mega-be/src/apis/project/get-project-list/get-project-list.query.schema.ts
+++ vsba-mega-be/src/apis/project/get-project-list/get-project-list.query.schema.ts
@@ -4,9 +4,11 @@ import { z } from "zod";
 extendZodWithOpenApi(z)
 
 export let get_project_list_query_schema = {
-    type: z.string().optional().openapi({ example: 'Project' }),
     page: z.string().optional().openapi({ example: '1' }),
     page_size: z.string().optional().openapi({ example: '8' }),
     sort_by: z.string().optional().openapi({ example: '' }),
     sort_order: z.string().optional().openapi({ example: '' }),
+    search: z.string().optional().openapi({ example: '' }),
+    type: z.string().optional().openapi({ example: '' }),
+    status: z.string().optional().openapi({ example: '' }),
 };
