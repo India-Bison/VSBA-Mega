56f5e71f0e811010a70e83abca9667f6c6ec712d
Rudra Belwalkar <rudra.belwalkar@thecontrast.in>
2025-05-08 09:53:09 +0530

    letest changes added in this branch

--- vsba-mega-fe/src/app/components/table-row/table-row.component.ts
+++ vsba-mega-fe/src/app/components/table-row/table-row.component.ts
@@ -1,6 +1,8 @@
 import { CommonModule, JsonPipe, NgClass, NgFor, NgIf, NgSwitch, UpperCasePipe } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, ContentChildren, ElementRef, HostListener, Input, QueryList } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { GlobalService } from '../../services/global.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-table-row',
@@ -10,7 +12,7 @@ import { FormsModule } from '@angular/forms';
 })
 export class TableRowComponent {
 
-  @Input() item: any;
+  @Input() items: any;
   @Input() columns: any;
 
   toggleExpand(item: any) {
@@ -21,5 +23,263 @@ export class TableRowComponent {
     item.selected = !item.selected;
     // you can emit event if needed
   }
+
+
+
+  @Input() params: any = {};
+  @Input() pagination: boolean = true
+  @Input() label: any = ''
+  @Input() description: any = ''
+  @Input() status: 'Border' | 'Dotted' = 'Border';
+  @Input()currentPage = 1;
+  @Input()totalPages = 1;
+  @Input()totalItems = 3;
+  @Input()itemsPerPage = 10;
+  active_tab = 'Project';
+  tabList = [
+    {
+      name: 'All',
+      action: () => {
+        this.active_tab = 'All',
+          console.log('All tab clicked')
+      }
+    },
+    {
+      name: 'Pending',
+      action: () => {
+        this.active_tab = 'Pending';
+        console.log('Pending tab clicked');
+      }
+    }, {
+      name: 'Approved',
+      action: () => {
+        this.active_tab = 'Approved';
+        console.log('Approved tab clicked');
+      }
+    }, {
+      name: 'Reject',
+      action: () => {
+        this.active_tab = 'Reject';
+        console.log('Reject tab clicked');
+      }
+    }, {
+      name: 'Disabled',
+      action: () => {
+        this.active_tab = 'Disabled';
+        console.log('Disabled tab clicked');
+      }
+    },
+    {
+      name: 'Drafts',
+      action: () => {
+        this.active_tab = 'Drafts';
+        console.log('Drafts tab clicked');
+      }
+    },
+  ];
+
+  @ContentChildren('*', { descendants: true, read: ElementRef }) projectedContent!: QueryList<ElementRef>;
+
+  isContentEmpty: boolean = true;
+
+  get uniqueItems() {
+    return this.items.filter((item: { key: string }, index: number, self: { key: string }[]) =>
+      index === self.findIndex((t: { key: string }) => t.key === item.key)
+    );
+  }
+  
+  
+
+  ngAfterContentInit() {
+    // Check if there are any content children
+    this.isContentEmpty = this.projectedContent.length === 0;
+  }
+
+  // State to track which column is expanded per row
+  expandedState: { [rowIndex: number]: string | null } = {};
+
+
+  constructor(public gs: GlobalService, public router : Router, public ar:ActivatedRoute) { }
+  ngOnInit(): void {
+    // this.items = this.gs.items
+     this.loadDummyData();
+    console.log(this.items, 'Items in List Component');
+  }
+  sortData(key: string, order: 'asc' | 'desc') {
+    this.router.navigate([], {
+      relativeTo: this.ar,
+      queryParams: {
+        sort_by: key,
+        sort_order: order
+      },
+      queryParamsHandling: 'merge',
+    });
+  }
+
+  // Dummy data with children
+  loadDummyData() {
+    this.items = [
+      {
+        id: 1,
+        type: 'Project',
+        name: 'Rudra',
+        resource_type: 'Computer Labs, Classrooms, swimming pool',
+        slot_type: 'Full Day',
+        startdate_enddate: '12/03/2025 - 25/03/2025',
+        status: 'Rejected',
+        children: [
+          {
+            id: 4,
+            type: 'Project',
+            name: 'Bhavesh',
+            resource_type: 'Computer Labs, Classrooms, swimming pool',
+            slot_type: 'Full Day',
+            startdate_enddate: '12/03/2025 - 25/03/2025',
+            status: 'Pending',
+             children: [
+          {
+            id: 4,
+            type: 'Project',
+            name: 'Bhavesh',
+            resource_type: 'Computer Labs, Classrooms, swimming pool',
+            slot_type: 'Full Day',
+            startdate_enddate: '12/03/2025 - 25/03/2025',
+            status: 'Pending',
+          },
+          {
+            id: 5,
+            type: 'Project',
+            name: 'Arun',
+            resource_type: 'Computer Labs, Classrooms, swimming pool',
+            slot_type: 'Full Day',
+            startdate_enddate: '12/03/2025 - 25/03/2025',
+            status: 'Pending'
+          },
+        ]
+          },
+          {
+            id: 5,
+            type: 'Project',
+            name: 'Arun',
+            resource_type: 'Computer Labs, Classrooms, swimming pool',
+            slot_type: 'Full Day',
+            startdate_enddate: '12/03/2025 - 25/03/2025',
+            status: 'Pending'
+          },
+        ]
+      },
+      {
+        id: 2,
+        type: 'Project',
+        name: 'Bhavesh',
+        resource_type: 'Computer Labs, Classrooms, swimming pool',
+        slot_type: 'Full Day',
+        startdate_enddate: '12/03/2025 - 25/03/2025',
+        status: 'Pending',
+        children: [
+          {
+            id: 4,
+            type: 'Project',
+            name: 'Bhavesh',
+            resource_type: 'Computer Labs, Classrooms, swimming pool',
+            slot_type: 'Full Day',
+            startdate_enddate: '12/03/2025 - 25/03/2025',
+            status: 'Pending'
+          },
+        ]
+      },
+      {
+        id: 3,
+        type: 'Project',
+        name: 'Mayur',
+        resource_type: 'Computer Labs, Classrooms, swimming pool',
+        slot_type: 'Full Day',
+        startdate_enddate: '12/03/2025 - 25/03/2025',
+        status: 'Approved',
+        children: [
+          {
+            type: 'Project',
+            name: 'Shreeram',
+            resource_type: 'Computer Labs, Classrooms, swimming pool',
+            slot_type: 'Full Day',
+            startdate_enddate: '12/03/2025 - 25/03/2025',
+            status: 'Approved',
+          }
+        ]
+      }
+    ];
+  }
+
+  // Toggle sub-column expansion per row and column
+  toggleSubColumn(rowIndex: number, columnKey: string): void {
+    if (this.expandedState[rowIndex] === columnKey) {
+      this.expandedState[rowIndex] = null;
+    } else {
+      this.expandedState[rowIndex] = columnKey;
+    }
+  }
+
+
+  // Pagination funcation
+  async handlePageChange(page: number) {
+    this.currentPage = page;
+  }
+
+  allSelected: boolean = false;
+
+  toggleAllSelection(): void {
+    this.allSelected = !this.allSelected;
+    this.items.forEach((item: any) => {
+      item.selected = this.allSelected;
+
+      if (item.children && item.children.length) {
+        item.children.forEach((child: any) => {
+          child.selected = this.allSelected;
+        });
+      }
+    });
+  }
+
+
+
+
+  toggleChildSelection(parent: any): void {
+    // If all children selected, set parent to selected
+    parent.selected = parent.children.every((child: any) => child.selected);
+
+    // Recheck header state
+    this.checkAllSelected();
+  }
+
+  checkAllSelected(): void {
+    this.allSelected = this.items.every((item: any) => {
+      const childrenSelected = item.children?.every((child: any) => child.selected) ?? true;
+      return item.selected && childrenSelected;
+    });
+  }
+
+
+  redirect_to_project_form(id: any) {
+    this.router.navigate(['/project/form'], { queryParams: { type: 'Project', parent_id: id, } });
+  }
+
+  menuVisibleIndex: number | string | null = null;
+
+  toggleMenu(index: number | string): void {
+    this.menuVisibleIndex = this.menuVisibleIndex === index ? null : index;
+  }
+  
+
+@HostListener('document:click', ['$event'])
+onDocumentClick(event: MouseEvent) {
+  this.menuVisibleIndex = null;
+}
+
+isDropdownOpen: number | null = null;
+
+toggleDropdown(index: number) {
+  this.isDropdownOpen = this.isDropdownOpen === index ? null : index;
+}
+
 
 }
