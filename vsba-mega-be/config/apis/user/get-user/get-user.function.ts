dcffa9013e676dc4c2075457bb45ea00b72efc22
shubham-salunkhe-tc <shubham.salunkhe@thecontrast.in>
2025-05-13 14:57:34 +0530

    keycloak basic

--- vsba-mega-be/config/apis/user/get-user/get-user.function.ts
+++ vsba-mega-be/config/apis/user/get-user/get-user.function.ts
@@ -5,22 +5,21 @@ import { User } from "@config/models/user.model"
 
 let get_user_function = async (data: get_user_function_params, transaction: Transaction): Promise<get_user_function_return | Error_Interface> => {
 
-    // let user = await User.findOne({
-    //     where: { email_id: data.email_id },
-    //     attributes: ['keyclock_user_id', 'keycloak_user_name', 'role'],
-    //     include: [], transaction
-    // })
-    // if (!user) {
-    //     return {
-    //         code: 200,
-    //         message: `User with Email ${data.email_id} not found!`
-    //     }
-    // }
+    let user = await User.findOne({
+        where: { email_id: data.email_id },
+        attributes: ['keyclock_user_id', 'keycloak_user_name', 'role'],
+        include: [], transaction
+    })
+    if (!user) {
+        return {
+            code: 200,
+            message: `User with Email ${data.email_id} not found!`
+        }
+    }
     return {
         code: 200,
         message: 'User fetched Successfully',
-        // data: user
-        data: {}
+        data: user
     }
 }
 
